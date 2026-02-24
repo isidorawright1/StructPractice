ead3257cf159f1a34a5a379fa202b4aaa2b67a75
isidora wright <isidora.wright.1@gmail.com>
2026-02-23 19:35:36 -0700

    start

---
+++ src/main.rs
@@ -0,0 +1,24 @@
+//import libraries/modules
+use std::io;
+
+//define struct
+struct Student {
+    name: String,
+    grades: Vec<u32>,
+    id: u32,
+}
+
+fn main() {
+
+}
+
+//Tests
+//tes to add a new student
+//remove a student
+//check passing grade
+//check failing grade
+#[cfg(test)]
+
+mod tests {
+    use super::*;
+}
